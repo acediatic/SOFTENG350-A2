4303a4c65c08dbcfd26fbc8243131ea10e640063
Adam Sinclair <hello@adamsinclair.dev>
2021-05-30 13:57:50 +1200

    refactor: naming and structure in line with brief.

---
+++ bdp-site/scripts.js
@@ -0,0 +1,14 @@
+const modal = document.getElementById("bookclubrego");
+const modal_background = document.getElementById("modal-background");
+const btn = document.getElementById("registerModalBtn");
+
+const openModal = () => {
+  modal.style.visibility = "visible";
+  modal_background.style.visibility = "visible";
+  btn.style.transition = "all 0.2s";
+};
+const closeModal = () => {
+  btn.style.transition = "all 0.01s";
+  modal.style.visibility = "hidden";
+  modal_background.style.visibility = "hidden";
+};
