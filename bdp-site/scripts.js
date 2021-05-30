95ce7dc2a8d23c9cb0e1b98ea1f1b4960105880c
Adam Sinclair <hello@adamsinclair.dev>
2021-05-30 15:22:15 +1200

    fixed keyboard nav modal

--- bdp-site/scripts.js
+++ bdp-site/scripts.js
@@ -6,9 +6,12 @@ const openModal = () => {
   modal.style.visibility = "visible";
   modal_background.style.visibility = "visible";
   btn.style.transition = "all 0.2s";
+  document.getElementById("givenName").focus();
 };
+
 const closeModal = () => {
   btn.style.transition = "all 0.01s";
   modal.style.visibility = "hidden";
   modal_background.style.visibility = "hidden";
+  document.activeElement.blur();
 };
